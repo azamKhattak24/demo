cce7a72634d0fcb6c3625e31503bf0086a02ee2b
Azam Khan <azamkhtk@gmail.com>
2022-01-01 22:50:54 +0500

    App Structure

--- components/home.js
+++ components/home.js
@@ -1,4 +1,4 @@
-import  React ,{Component} from 'react';
+import  React  from 'react';
 import { Text, View, 
         StyleSheet,Button, 
         SafeAreaView, TouchableOpacity,
@@ -11,48 +11,42 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 const Home =({navigation})=> {
   return (
     <SafeAreaView style={styles.container} >
-   
-    <View style={styles.body}>
-     <ImageBackground style={styles.image, styles.body} source={require('../assets/back.jpg')}>
-     
-    <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate("MobileAds")}}>
+
+      <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate("MobileAds")}}>
          <View style={styles.touchView}>
      <Text style ={{fontWeight:'bold', marginRight:'5%'}}>Mobiles</Text>
     <Icon name='mobile' size={24}/>
    </View>
         </TouchableOpacity>
 
-
-    <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate("MyAds")}}>
+        <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate("MyAds")}}>
          <View style={styles.touchView}>
      <Text style ={{fontWeight:'bold', marginRight:'5%'}}>My Ads</Text>
     <Icon name='table' size={24}/>
    </View>
         </TouchableOpacity>
-        
+
         <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate("Ad")}}>
          <View style={styles.touchView}>
      <Text style ={{fontWeight:'bold', marginRight:'5%'}}>Favourite</Text>
     <Icon name='heart' size={24} color='red'/>
    </View>
         </TouchableOpacity>
-        
-       <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate("PostAD")}}>
+
+        <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate("PostAD")}}>
          <View style={styles.touchView}>
      <Text style ={{fontWeight:'bold', marginRight:'5%'}}>Post An Ad</Text>
     <Icon name='clipboard' size={24}/>
    </View>
         </TouchableOpacity>
-        
-        <TouchableOpacity style={styles.button, styles.touchView} onPress={()=>{navigation.navigate("Ad")}}>
+
+        <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate("PostAD")}}>
          <View style={styles.touchView}>
      <Text style ={{fontWeight:'bold', marginRight:'5%'}}>Profile</Text>
-    <Icon name='user-circle' size={24} />
+    <Icon name='user-circle' size={24}/>
    </View>
         </TouchableOpacity>
-    </ImageBackground>
-        
-    </View>
+
     </SafeAreaView>
   );
 };
@@ -60,7 +54,10 @@ const Home =({navigation})=> {
 const styles = StyleSheet.create({
 
  container : {
-  flex: 1
+  flex: 1,
+  alignItems: 'center',
+  justifyContent:'center',
+  backgroundColor:'black',
   },
  
   body : {
@@ -74,14 +71,15 @@ const styles = StyleSheet.create({
   button: {
     borderRadius: 10,
     marginBottom: 20,
+    alignItems: 'center', 
     shadowColor: '#303838',
     shadowOffset: { width: 0, height: 5 },
     shadowRadius: 10,
     shadowOpacity: 0.35,
     width: '50%',
     height: '15%',
-    backgroundColor:'pink'
-    
+    backgroundColor:'pink',
+    justifyContent:'center'
     
   },
 
@@ -94,7 +92,7 @@ const styles = StyleSheet.create({
   },
 
   touchView:{
-  position: 'absolute', 
+ // position: 'absolute', 
   justifyContent: 'center', 
   alignItems: 'center', 
   flexDirection:'row'}
